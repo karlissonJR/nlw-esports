1f5a50c6d45be96b5a91473a5a2b93107c1879d6
karlissonJR <karlissonj@gmail.com>
2022-09-13 14:41:18 -0300

    configuração da fonte

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,26 @@
-import { StatusBar } from 'react-native';
+import { StatusBar } from 'react-native'
 
-import { Background } from './src/components/Background';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+
+import { Background } from './src/components/Background'
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+  
   return (
     <Background>
       <StatusBar
@@ -10,6 +28,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+
+      {fontsLoaded ? <Home /> : <Loading />}
+
     </Background>
   );
 }
