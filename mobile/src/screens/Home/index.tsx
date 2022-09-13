a58579bea69277f6c3f92424d5e11f1269bbdac0
karlissonJR <karlissonj@gmail.com>
2022-09-13 15:44:38 -0300

    lista de jogos

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
+
+import { GAMES } from '../../utils/games'
 
 import { styles } from './styles';
 
@@ -17,6 +19,19 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subTitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
       />
     </View>
   );
--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -5,10 +5,16 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center'
   },
+  
   logo: {
     width: 214,
     height: 120,
     marginTop: 74,
     marginBottom: 48
+  },
+
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64
   }
 });
