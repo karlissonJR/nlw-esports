31589bcea22b1038c0cb25e262d037163bfd33f8
karlissonJR <karlissonj@gmail.com>
2022-09-17 12:58:31 -0300

    criação de anúncios através da api

--- web/src/components/CreateAdvertisementModal/index.tsx
+++ web/src/components/CreateAdvertisementModal/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState, FormEvent } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { GameController, Check } from 'phosphor-react'
 import * as Checkbox from '@radix-ui/react-checkbox'
@@ -14,10 +14,30 @@ interface Props {
 function CreateAdvertisementModal({games}: Props) {
 
     const [weekDays, setWeekDays] = useState<string[]>([])
+    const [useVoiceChanel, setUseVoiceChanel] = useState<Checkbox.CheckedState>(false)
 
-    useEffect(() => {
-        console.log('OLOKO = ', games)
-    }, [games])
+    function handleCreateAdvertisement(event: FormEvent) {
+      event.preventDefault()
+
+      const formData = new FormData(event.target as HTMLFormElement)
+      const entriesData = Object.fromEntries(formData)
+
+      const data = {
+        ...entriesData,
+        gameId: Number(entriesData.gameId),
+        yearsPlaying: Number(entriesData.yearsPlaying),
+        weekDays: weekDays.map(item => parseInt(item)),
+        useVoiceChanel
+      }
+
+      fetch('http://127.0.0.1:8080/ads', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }).then(response => console.log(response.status))
+    }
 
     return (
         <Dialog.Portal>
@@ -26,11 +46,12 @@ function CreateAdvertisementModal({games}: Props) {
           <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
             <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
 
-            <form className="mt-8 flex flex-col gap-4">
+            <form className="mt-8 flex flex-col gap-4" onSubmit={handleCreateAdvertisement}>
               <div className="flex flex-col gap-2">
                 <label htmlFor="game" className="font-semibold">Qual o game?</label>
                 <select
-                    id="game"
+                    id="gameId"
+                    name="gameId"
                     className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none"
                     defaultValue=""
                 >
@@ -46,6 +67,7 @@ function CreateAdvertisementModal({games}: Props) {
                 <label htmlFor="name" className="font-semibold">Seu nome (ou o nickname)?</label>
                 <Input
                   id="name"
+                  name="name"
                   placeholder="como te chamam dentro do game?"
                 />
               </div>
@@ -53,11 +75,21 @@ function CreateAdvertisementModal({games}: Props) {
               <div className="grid grid-cols-2 gap-6">
                 <div className="flex flex-col gap-2">
                   <label htmlFor="yearsPlaying">Joga há qunatos anos?</label>
-                  <Input id="yearsPlayng" type="number" placeholder="Tudo bem ser ZERO" />
+                  <Input
+                    id="yearsPlaying"
+                    name="yearsPlaying"
+                    type="number"
+                    placeholder="Tudo bem ser ZERO"
+                  />
                 </div>
                 <div className="flex flex-col gap-2">
                   <label htmlFor="discord">Qual seu discord?</label>
-                  <Input id="discord" type="text" placeholder="Usuario#0000" />
+                  <Input
+                    id="discord"
+                    name="discord"
+                    type="text"
+                    placeholder="Usuario#0000"
+                  />
                 </div>
               </div>
 
@@ -66,6 +98,7 @@ function CreateAdvertisementModal({games}: Props) {
                   <label htmlFor="weekDays">Quando costuma jogar?</label>
                   
                     <ToggleGroup.Root
+                        id="weekDays"
                         type="multiple"
                         className="grid grid-cols-4 gap-2"
                         value={weekDays}
@@ -84,18 +117,32 @@ function CreateAdvertisementModal({games}: Props) {
                 <div className="flex flex-col gap-2 flex-1">
                   <label htmlFor="hourStart">Qual horário do dia?</label>
                   <div className="grid grid-cols-2 gap-2">
-                    <Input id="hourStart" type="time" placeholder="De" />
-                    <Input id="HourEnd" type="time" placeholder="Até" />
+                    <Input
+                      id="hourStart"
+                      name="hourStart"
+                      type="time"
+                      placeholder="De"
+                    />
+                    <Input
+                      id="hourEnd"
+                      name="hourEnd"
+                      type="time"
+                      placeholder="Até"
+                    />
                   </div>
                 </div>
               </div>
 
               <label className="mt-2 flex gap-2 text-sm items-center cursor-pointer">
-                <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900 ">
+                <Checkbox.Root
+                  name="useVoiceChanel"
+                  id="useVoiceChanel"
+                  className="w-6 h-6 p-1 rounded bg-zinc-900"
+                  checked={useVoiceChanel}
+                  onCheckedChange={checked => setUseVoiceChanel(checked)}
+                >
                     <Checkbox.Indicator>
-                        <Check
-                            className="w-4 h-4 text-emerald-400"
-                        />
+                        <Check className="w-4 h-4 text-emerald-400" />
                     </Checkbox.Indicator>
                 </Checkbox.Root>
                 Costumo me conectar ao chat de voz
