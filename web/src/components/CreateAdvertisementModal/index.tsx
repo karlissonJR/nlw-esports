9688db8dfd54c5a23b7342351739de19c4d67cdc
karlissonJR <karlissonj@gmail.com>
2022-09-17 08:37:49 -0300

    modal bonitinho

---
+++ web/src/components/CreateAdvertisementModal/index.tsx
@@ -0,0 +1,125 @@
+import { useEffect, useState } from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
+import { GameController, Check } from 'phosphor-react'
+import * as Checkbox from '@radix-ui/react-checkbox'
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+
+import Input from '../../components/Form/Input'
+import { Game } from '../../App'
+
+interface Props {
+    games: Game[]
+}
+
+function CreateAdvertisementModal({games}: Props) {
+
+    const [weekDays, setWeekDays] = useState<string[]>([])
+
+    useEffect(() => {
+        console.log('OLOKO = ', games)
+    }, [games])
+
+    return (
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
+            <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
+
+            <form className="mt-8 flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="game" className="font-semibold">Qual o game?</label>
+                <select
+                    id="game"
+                    className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none"
+                    defaultValue=""
+                >
+                    <option disabled value="">Selecione o game que deseja jogar</option>
+
+                    {games.map((game: Game) => (
+                        <option key={game.id} value={game.id}>{game.title}</option>
+                    ))}
+                </select>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label htmlFor="name" className="font-semibold">Seu nome (ou o nickname)?</label>
+                <Input
+                  id="name"
+                  placeholder="como te chamam dentro do game?"
+                />
+              </div>
+
+              <div className="grid grid-cols-2 gap-6">
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="yearsPlaying">Joga há qunatos anos?</label>
+                  <Input id="yearsPlayng" type="number" placeholder="Tudo bem ser ZERO" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="discord">Qual seu discord?</label>
+                  <Input id="discord" type="text" placeholder="Usuario#0000" />
+                </div>
+              </div>
+
+              <div className="flex gap-6">
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="weekDays">Quando costuma jogar?</label>
+                  
+                    <ToggleGroup.Root
+                        type="multiple"
+                        className="grid grid-cols-4 gap-2"
+                        value={weekDays}
+                        onValueChange={setWeekDays}
+                    >
+                        <ToggleGroup.Item value="0" className={`w-8 h-8 rounded ${weekDays.includes('0') ? 'bg-violet-500': 'bg-zinc-900'}`} title="Domingo">D</ToggleGroup.Item>
+                        <ToggleGroup.Item value="1" className={`w-8 h-8 rounded ${weekDays.includes('1') ? 'bg-violet-500': 'bg-zinc-900'}`} title="Segund">S</ToggleGroup.Item>
+                        <ToggleGroup.Item value="2" className={`w-8 h-8 rounded ${weekDays.includes('2') ? 'bg-violet-500': 'bg-zinc-900'}`} title="Terça">T</ToggleGroup.Item>
+                        <ToggleGroup.Item value="3" className={`w-8 h-8 rounded ${weekDays.includes('3') ? 'bg-violet-500': 'bg-zinc-900'}`} title="Quarta">Q</ToggleGroup.Item>
+                        <ToggleGroup.Item value="4" className={`w-8 h-8 rounded ${weekDays.includes('4') ? 'bg-violet-500': 'bg-zinc-900'}`} title="Quinta">Q</ToggleGroup.Item>
+                        <ToggleGroup.Item value="5" className={`w-8 h-8 rounded ${weekDays.includes('5') ? 'bg-violet-500': 'bg-zinc-900'}`} title="Sexta">S</ToggleGroup.Item>
+                        <ToggleGroup.Item value="6" className={`w-8 h-8 rounded ${weekDays.includes('6') ? 'bg-violet-500': 'bg-zinc-900'}`} title="Sábado">S</ToggleGroup.Item>
+                    </ToggleGroup.Root>
+                 
+                </div>
+                <div className="flex flex-col gap-2 flex-1">
+                  <label htmlFor="hourStart">Qual horário do dia?</label>
+                  <div className="grid grid-cols-2 gap-2">
+                    <Input id="hourStart" type="time" placeholder="De" />
+                    <Input id="HourEnd" type="time" placeholder="Até" />
+                  </div>
+                </div>
+              </div>
+
+              <label className="mt-2 flex gap-2 text-sm items-center cursor-pointer">
+                <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900 ">
+                    <Checkbox.Indicator>
+                        <Check
+                            className="w-4 h-4 text-emerald-400"
+                        />
+                    </Checkbox.Indicator>
+                </Checkbox.Root>
+                Costumo me conectar ao chat de voz
+              </label>
+
+              <footer className="mt-4 flex justify-end gap-4">
+                <Dialog.Close
+                  type="button"
+                  className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600"
+                >
+                  Cancelar
+                </Dialog.Close>
+                <button
+                  type="submit"
+                  className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+                >
+                  <GameController size={24} /> Encontrar duo
+                </button>
+              </footer>
+            </form>
+            
+          </Dialog.Content>
+        </Dialog.Portal>
+    )
+}
+
+export default CreateAdvertisementModal
